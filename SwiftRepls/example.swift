ea1705145001fc2974dc625fbe3f21851ad413ac
Matthew Collins <m.collins@etoncollege.org.uk>
2023-10-31 13:16:04 +0000

    added a new example file

---
+++ SwiftRepls/example.swift
@@ -0,0 +1,12 @@
+//
+//  example.swift
+//  SwiftRepls
+//
+//  Created by Collins, Matthew - MC on 31/10/2023.
+//
+
+import Foundation
+
+func helloMaker(name: String) {
+    print("Hello \(name)")
+}
