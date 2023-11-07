4bb734db2a19127ff82a8b826dade312a6f82983
Matthew Collins <m.collins@etoncollege.org.uk>
2023-11-07 13:17:36 +0000

    added test cases

---
+++ SwiftRepls/Algorithm.swift
@@ -0,0 +1,19 @@
+//
+//  Algorithm.swift
+//  SwiftRepls
+//
+//  Created by Collins, Matthew - MC on 07/11/2023.
+//
+
+import Foundation
+
+class Algorithm {
+    
+    func linearSearch(data: [Int], item: Int) -> Bool {
+        
+        if data.count == 0 {
+            return false
+        }
+        return true
+    }
+}
